f01c215b295ed4220877ab68b25a1d6019d1aeed
ryuto <rryuto5210@gmail.com>
2019-09-20 19:57:36 +0900

    First commit

---
+++ README.md
@@ -0,0 +1,50 @@
+# care-label-app
+画像認識による洗濯表示の識別
+
+## Description
+画像から洗濯表示を識別し、意味を表示する
+
+>洗濯表示とは？  
+衣服についたタグのマークのこと。  
+洗濯や乾燥の方法、アイロンのかけ方やクリーニングの方法などが示されている。  
+全41種類。世界共通。
+
+### WEBアプリケーション
+- TOP
+![screencapture-care-label-herokuapp-2019-09-17-18_58_57](https://user-images.githubusercontent.com/20394831/65031999-44356480-d97d-11e9-976b-a46833b3f78b.png)
+
+- 予測結果表示
+![screencapture-care-label-herokuapp-predict-2019-09-17-19_00_41](https://user-images.githubusercontent.com/20394831/65032112-7a72e400-d97d-11e9-84e4-53c73b5a4c14.png)
+
+
+## Features
+- 洗濯表示の識別
+- 意味出力
+
+## Requirement
+画像の水増し python 3.6~（f文字列）
+
+WERアプリ
+[requirements.txt](/app/requirements.txt)
+
+## Usage
+### 画像の水増し
+1枚から約1500枚に水増し。明るさ、ノイズ、回転、移動（上下左右斜め）の加工を行う。
+
+1. [predict/train-seeds](/cnn/train-seeds/)下の該当するフォルダに画像をおく
+2. [all-ver2.ipynb](/app/all-ver2.ipynb)を実行する  
+(windowsの場合は、[all-ver2-win.ipynb](/cnn/all-ver2-win.ipynb))
+3. [predict/train](/cnn/train/)下の該当するフォルダに水増しされた画像が出力される
+
+
+上記の方法は、一括で水増しする場合。
+1枚1枚指定して水増しする場合は、[all-file.ipynb](/cnn/all-file.ipynb)を実行する。
+
+### 学習
+[predict.py](/cnn/predict.py)を実行する。
+
+モデル、重み、学習履歴、学習過程の図が保存される。
+
+### WEBアプリケーション
+[app.py](/app/app.py)を実行する。  
+localhost:5000から確認できる。  
---
+++ cnn/predict.py
@@ -0,0 +1,152 @@
+# coding:utf-8
+
+import keras
+from keras.utils import np_utils
+from keras.models import Sequential
+from keras.layers.convolutional import Conv2D, MaxPooling2D
+from keras.layers.core import Dense, Dropout, Activation, Flatten
+import numpy as np
+from sklearn.model_selection import train_test_split
+from PIL import Image
+import glob
+
+folder = ['95',
+        '50',
+        'hanging_dry',
+        'not_bleachable',
+        'wetcleaning_weak',
+        '60',
+        'wetcleaning_ok',
+        'donot_drycleaning',
+        'weetcleaning_very_weak',
+        '30_very_weak',
+        '40_very_weak',
+        'ironing_upto200',
+        'not_washable',
+        'donot_wetcleaning',
+        'drycleaning_F',
+        'bleachable',
+        'flat_dry_shade',
+        'drycleaning_F_weak',
+        'flat_dry_wet',
+        'drycleaning_P_weak',
+        'tumble_dry_upto60',
+        'ironing_upto150',
+        'donot_tumble_dry',
+        '30',
+        'hanging_dry_wet',
+        'flat_dry_wetshade',
+        '40_weak',
+        'donot_ironing',
+        'flat_dry',
+        '60_weak',
+        'hanging_dry_shade',
+        'bleachable_oxygen',
+        '30_weak',
+        'drycleaning_P',
+        'hand-wash',
+        '70',
+        'hanging_dry_wetshade',
+        'tumble_dry_upto80',
+        '40',
+        '50_weak',
+        'ironing_upto110']
+
+image_size = 50
+
+X = []
+Y = []
+for index, name in enumerate(folder):
+    dir = "./train/" + name
+    files = glob.glob(dir + "/*.png")
+    for i, file in enumerate(files):
+        image = Image.open(file)
+        image = image.convert("RGB")
+        image = image.resize((image_size, image_size))
+        data = np.asarray(image)
+        X.append(data)
+        Y.append(index)
+
+X = np.array(X)
+Y = np.array(Y)
+
+X = X.astype('float32')
+X = X / 255.0
+
+print(X)
+
+Y = np_utils.to_categorical(Y, len(folder))
+
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.20)
+
+
+# CNNを構築
+model = Sequential()
+
+model.add(Conv2D(32, (3, 3), padding='same',input_shape=X_train.shape[1:]))
+model.add(Activation('relu'))
+model.add(Conv2D(32, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Conv2D(64, (3, 3), padding='same'))
+model.add(Activation('relu'))
+model.add(Conv2D(64, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Flatten())
+model.add(Dense(512))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(len(folder)))
+model.add(Activation('softmax'))
+
+# コンパイル
+model.compile(loss='categorical_crossentropy',optimizer='SGD',metrics=['accuracy'])
+
+#訓練
+history = model.fit(X_train, y_train, epochs=3, validation_data=(X_test, y_test))
+
+#評価 & 評価結果出力
+print(model.evaluate(X_test, y_test))
+
+# [0.011538714563043074, 0.9979025710419486]
+
+# モデルの保存
+open('and.json',"w").write(model.to_json())
+
+# 学習済みの重みを保存
+model.save_weights('and_weight.hdf5')
+
+import matplotlib.pyplot as plt
+
+
+def plot_history(history):
+    # 精度の履歴をプロット
+    plt.plot(history.history['acc'],"o-",label="accuracy")
+    plt.plot(history.history['val_acc'],"o-",label="val_acc")
+    plt.title('model accuracy')
+    plt.xlabel('epoch')
+    plt.ylabel('accuracy')
+    plt.legend(loc="lower right")
+    # plt.show()
+    plt.savefig("model_accuracy.png")
+
+    # 損失の履歴をプロット
+    plt.plot(history.history['loss'],"o-",label="loss",)
+    plt.plot(history.history['val_loss'],"o-",label="val_loss")
+    plt.title('model loss')
+    plt.xlabel('epoch')
+    plt.ylabel('loss')
+    plt.legend(loc='lower right')
+    # plt.show()
+    plt.savefig("model_loss.png")
+
+plot_history(history)
+
+import pickle
+with open("history.pickle", mode='wb') as f:
+    pickle.dump(history.history, f)
